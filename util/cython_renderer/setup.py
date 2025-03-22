9d3e23554dc749bf02afab73d607944033a63dea
devy52 <108571763+devy52@users.noreply.github.com>
2025-03-22 18:46:47 +0530

    Update setup.py

--- util/cython_renderer/setup.py
+++ util/cython_renderer/setup.py
@@ -3,17 +3,29 @@ python setup.py build_ext -i
 to compile
 '''
 
-# setup.py
-from distutils.core import setup, Extension
+import sys
+from setuptools import setup, Extension
 from Cython.Build import cythonize
-from Cython.Distutils import build_ext
 import numpy
 
+# Enforce Python 3
+if sys.version_info.major < 3:
+    raise RuntimeError("This package requires Python 3.x")
+
+# Define the extension
+extension = Extension(
+    "mesh_core_cython",  # Output module name
+    sources=["mesh_core_cython.pyx", "mesh_core.cpp"],  # Cython and C++ sources
+    language="c++",  # Specify C++ language
+    include_dirs=[numpy.get_include()],  # NumPy headers
+    define_macros=[('NPY_NO_DEPRECATED_API', 'NPY_2_0_API_VERSION')]  # NumPy 2.0 API
+)
+
 setup(
-	name = 'mesh_core_cython',
-    cmdclass={'build_ext': build_ext},
-    ext_modules=[Extension("mesh_core_cython",
-                 sources=["mesh_core_cython.pyx", "mesh_core.cpp"],
-                 language='c++',
-                 include_dirs=[numpy.get_include()])],
+    name="mesh_core_cython",
+    ext_modules=cythonize(
+        [extension],
+        compiler_directives={'language_level': '3str'}  # Python 3 string semantics
+    ),
+    python_requires=">=3.6",  # Minimum Python 3 version
 )
